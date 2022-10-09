fb6f5fb304d386ffc2f308aa7131a5ab2ee495ad
unknown <ot250013@yandex.ru>
2022-10-09 22:00:02 +0300

    Add Task_3 Ok and Task_4 OK

--- Home_Work/Seminar_1/Task_3.py
+++ Home_Work/Seminar_1/Task_3.py
@@ -4,6 +4,22 @@
 #
 # 1 -> x > 0, y > 0
 
+x = int(input("Введите номер четверти: "))
+if x == 1:
+    print("Х > 0, Y > 0")
+elif x == 2:
+    print("X < 0, Y > 0")
+elif x == 3:
+    print("X < 0, Y < 0")
+elif x == 4:
+    print("X > 0, Y < 0")
+elif x != 1 and x != 2 and x != 3 and x != 4:
+    print(f"Некорректный ввод данных, четверти с номером {x} не существует")
+
+
+# Сделал решение в обратную чторону)
+
+
 point_X = int(input("Введите значение Х: "))
 point_Y = int(input("Введите значение Y: "))
 
@@ -15,3 +31,5 @@ elif point_X < 0 and point_Y < 0:
     print("Третья четверть")
 elif point_X > 0 and point_Y < 0:
     print("Четвертая четверть")
+elif point_X == 0 and point_Y == 0:
+    print("точка находится на оси координат Х и Y")
--- Home_Work/Seminar_1/Task_4.py
+++ Home_Work/Seminar_1/Task_4.py
@@ -3,9 +3,13 @@
 # а на выходе показывает все чётные числа от 1 до N.
 # 5 -> 2, 4
 # 8 -> 2, 4, 6, 8
+import math
+
 
 number = int(input("Введите значение N: "))
 n = range(1, number + 1)
 for i in n:
     if i % 2 == 0:
-        print(i)
+        print(i, end=" ")
+
+
