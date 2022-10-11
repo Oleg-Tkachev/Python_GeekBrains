a71f778e0839ec865f70da1ee109c47483774e07
unknown <ot250013@yandex.ru>
2022-10-12 01:41:07 +0300

    Add Task_2 progress

--- Home_Work/Seminar_2/Task_2.py
+++ Home_Work/Seminar_2/Task_2.py
@@ -1,3 +1,9 @@
-
 # Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
 
+print("X | Y | Z | ¬(x∧y)∨z")
+print("--------------------")
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            sum = (int(not (x and y) or z))
+            print(f'{x} | {y} | {z} |    {sum} ')
