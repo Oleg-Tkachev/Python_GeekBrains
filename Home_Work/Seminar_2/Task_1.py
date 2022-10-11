c7f46c1879c06ea30cb2483db873ed4fdf45f261
unknown <ot250013@yandex.ru>
2022-10-12 00:40:51 +0300

    Add Task_1 OK

--- Home_Work/Seminar_2/Task_1.py
+++ Home_Work/Seminar_2/Task_1.py
@@ -4,3 +4,27 @@
 #
 # N = 4 -> [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+n = int(input('Введите число N: '))
+
+def Factorial(number):
+    if number == 0:
+        return 1
+    factorial = 1
+    for i in range(1, number + 1):
+        factorial *= i
+    return factorial
+
+print(Factorial(n))
+
+
+
+# Второй способ через рекурсию
+
+def Factorial(number):
+    if number == 0:
+        return 1
+    return number * Factorial(number - 1)
+
+print(Factorial(n))
+
+
