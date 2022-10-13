c4a42da450baadfa3fda4545e88dd5fab27035a2
unknown <ot250013@yandex.ru>
2022-10-14 02:03:30 +0300

    ADD_Task 2of4 in progress

--- Home_Work/Seminar_2/ADD_Task(2).py
+++ Home_Work/Seminar_2/ADD_Task(2).py
@@ -1,4 +1,15 @@
-
 # Дан отсортированный массив, заполненный случайными вещественными числами.
 # Найдите ближайший к среднему арифметическому максимума и минимума элемент массива.
+
+
+from random import randint
+
+numbers = []
+for i in range(10):
+    numbers.append(randint(1, 50))
+numbers.sort()
+print(numbers)
+
+average = sum(numbers) / len(numbers)
+print('Среднее арифметическое', average)
 
