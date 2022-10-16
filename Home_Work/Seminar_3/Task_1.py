96f14296015923e7034e61acc16d2a1f79ac6840
unknown <ot250013@yandex.ru>
2022-10-16 23:20:44 +0300

    Task_1 OK

--- Home_Work/Seminar_3/Task_1.py
+++ Home_Work/Seminar_3/Task_1.py
@@ -1,3 +1,19 @@
 #
 # Создайте файл. Запишите в него N первых элементов последовательности Фибоначчи.
 # 6 –> 1 1 2 3 5 8
+
+n = int(input("Введите число N: "))
+
+
+def fibonacci(n):
+    a, b = 1, 1
+    with open('fibonacci.txt', 'w') as ouf:
+        for i in range(1, n + 1):
+            ouf.write(str(i) + ' –> ' + str(a) + '\n')
+            a, b = b, a + b
+
+print(fibonacci(n))
+
+
+
+
