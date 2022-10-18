21f794bef110abeb04a5be9b7e2f508a395454db
unknown <ot250013@yandex.ru>
2022-10-19 00:44:31 +0300

    Task_2 OK

--- Home_Work/Seminar_3/Task_2.py
+++ Home_Work/Seminar_3/Task_2.py
@@ -3,13 +3,13 @@
 #  Выведите все фрукты, названия которых начинаются на заданную букву.
 # а –> абрикос, авокадо, апельсин, айва.
 
-n = input("Введите букву: ")
+bykva = input("Введите первую букву фрукта: ")
 
-def find_frukt(n):
-    with open('frukt.txt', 'r', encoding='utf-8') as frukt:
-        fruits = [frukt.readline().strip().replace('\n', '') for i in frukt]  # Строчку подсказали коллеги.
-    for fruit in fruits:                                                      # частично не понимаю что здесь происходит(
-        if fruit and fruit[0] == n:
-            print(fruit)
 
-find_frukt(n)
+with open("frukt.txt", 'r', encoding='utf-8') as data:
+    fruit = data.readline().split(', ')
+for fr in fruit:
+    if fr[0].lower() == bykva.lower():
+        print(fr)
+
+
