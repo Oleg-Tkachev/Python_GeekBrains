a7c88926760968e81b0fade3899a5e9295c44400
unknown <ot250013@yandex.ru>
2022-10-19 01:58:16 +0300

    Task_1 OK

--- Home_Work/Seminar_4/Task_1.py
+++ Home_Work/Seminar_4/Task_1.py
@@ -2,4 +2,19 @@
 #  Напишите программу, которая составит список простых множителей числа N.
 # 60 -> 2, 2, 3, 5
 
+n = int(input("Введите число N: "))
 
+
+def list_multipliers_number(num):
+    divisor = 2
+    numbers = []
+    while divisor <= num:
+        if num % divisor == 0:
+            numbers.append(divisor)
+            num //= divisor
+        else:
+            divisor += 1
+    return numbers
+
+
+print(f"{n} --> {list_multipliers_number(n)}")
