ff275b12345805b3a87460f1a23e72aed1a63749
unknown <ot250013@yandex.ru>
2022-10-20 00:29:18 +0300

    Task_2 in progress

--- Home_Work/Seminar_4/Task_2.py
+++ Home_Work/Seminar_4/Task_2.py
@@ -11,10 +11,8 @@
 with open('icecream', 'r', encoding='utf-8') as inf:
 
     icecream_range = set(inf.readline().split(', '))
-    print(icecream_range)
 
     icecream_sklad = set(inf.readline().split(', '))
-    print(icecream_sklad)
 
     print(icecream_range - icecream_sklad)
 
