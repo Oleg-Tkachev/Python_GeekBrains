7a11676cbe4e5b0f070a70b99400df2524e152fc
unknown <ot250013@yandex.ru>
2022-10-20 00:24:18 +0300

    Task_2 in progress

--- Home_Work/Seminar_4/Task_2.py
+++ Home_Work/Seminar_4/Task_2.py
@@ -6,3 +6,15 @@
 # 2. «Сливочное», «Вафелька», «Сладкоежка»
 # Закончилось: «Бурёнка»
 
+
+
+with open('icecream', 'r', encoding='utf-8') as inf:
+
+    icecream_range = set(inf.readline().split(', '))
+    print(icecream_range)
+
+    icecream_sklad = set(inf.readline().split(', '))
+    print(icecream_sklad)
+
+    print(icecream_range - icecream_sklad)
+
