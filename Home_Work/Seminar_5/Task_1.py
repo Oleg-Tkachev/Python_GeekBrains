11687c5aa48e1d60150994d2c6d7cb9988cbb1df
unknown <ot250013@yandex.ru>
2022-10-25 23:58:03 +0300

    Task_1 OK

--- Home_Work/Seminar_5/Task_1.py
+++ Home_Work/Seminar_5/Task_1.py
@@ -1,5 +1,17 @@
-
 # Задайте список случайных чисел от 1 до 10,
 # выведите все элементы больше 5. Используйте для решения лямбда-функцию.
 # 2, 3, 4, 6, 7, 8 -> 6, 7, 8
 
+import random
+
+n = int(input("Введите количество элементов массива: "))
+numbers = [random.randint(1, 10) for i in range(n)]
+print(numbers)
+
+s = int(input("Напечатать элементы больше какой цифры?: "))
+
+
+def numbers_greater_five(numbers):
+    return list(filter(lambda x: x > s, numbers))
+
+print(numbers_greater_five(numbers))
