4ae1bac3cf33cb8a9dc10c4c22680e6883134ff5
unknown <ot250013@yandex.ru>
2022-10-26 01:52:59 +0300

    Task_2 OK

--- Home_Work/Seminar_5/Task_2.py
+++ Home_Work/Seminar_5/Task_2.py
@@ -1,6 +1,21 @@
-
 # Дан список случайных чисел.
 # Создайте список, в который попадают числа,
 # описывающие возрастающую последовательность. Порядок элементов менять нельзя.
 # [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
 
+
+import random
+
+numbers = [random.randint(1, 10) for i in range(10)]
+print(numbers)
+
+
+def increase_numbers(numbers):
+    new_num_up = [numbers[0]]
+    for i in numbers:
+        if i > max(new_num_up):
+            new_num_up.append(i)
+    return new_num_up
+
+
+print(increase_numbers(numbers))
