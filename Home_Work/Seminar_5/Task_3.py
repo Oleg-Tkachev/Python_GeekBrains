3ed33dbe85e2206bc7daa44b5e6c59ea07476ad9
unknown <ot250013@yandex.ru>
2022-10-26 02:09:01 +0300

    Task_3 in progress

--- Home_Work/Seminar_5/Task_3.py
+++ Home_Work/Seminar_5/Task_3.py
@@ -5,3 +5,17 @@
 # [1, 4, 2, 3, 4, 6, 1, 7] => 4 элемента совпадают.
 # Список уникальных элементов [1, 4, 2, 3, 6, 7]
 
+
+import random
+
+numbers = [random.randint(1, 10) for i in range(10)]
+print("Список из 10 случайных элементов от 1 до 10:")
+print(numbers)
+
+def filter_elements(numbers):
+    return list(filter(lambda x: numbers.count(x) == 1, numbers))
+
+print("Список уникальных элементов из списка выше:")
+
+print(filter_elements(numbers))
+
