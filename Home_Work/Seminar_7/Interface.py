6c5eb8f34e4595ee5eb88215a7e4a82e93c4bd64
unknown <ot250013@yandex.ru>
2022-11-14 01:07:06 +0300

    Create Interface Progress

---
+++ Home_Work/Seminar_7/Interface.py
@@ -0,0 +1,28 @@
+
+def creat_information():
+    information = []
+
+    first_name = input('Введите имя: ')
+    information.append(first_name)
+
+    last_name = input('Введите фамилию: ')
+    information.append(last_name)
+
+    phone_number = None
+    valid = False
+    while not valid:
+        try:
+            phone_number = input('Введите номер телефона +7: ')
+            if len(phone_number) != 10:
+                print('В номере телефона должно быть 10 цифр.')
+            else:
+                phone_number = int(phone_number)
+                valid = True
+        except:
+            print('Некорректный ввод')
+    information.append(phone_number)
+    remark = input('Добавьте описание: ')
+    information.append(remark)
+    return
+
+
