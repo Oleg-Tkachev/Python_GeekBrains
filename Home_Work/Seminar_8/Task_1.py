6a4d5e18dc4a3d1f1a7e9f0d5ae69432b92d85e5
unknown <ot250013@yandex.ru>
2022-11-24 00:20:14 +0300

    Task_1 ....

--- Home_Work/Seminar_8/Task_1.py
+++ Home_Work/Seminar_8/Task_1.py
@@ -3,15 +3,33 @@
 # Каждой группе отведена своя строка.
 # Определите группу с наилучшим средним баллом.
 
+import operator
+from functools import reduce
 from random import randint
+from statistics import mean
+import numpy as np
+from urllib3.connectionpool import xrange
 
 count_group = int(input('Введите количество групп: '))
 students_grades = [0] * count_group
 grade_average = []
 
 for i in range(len(students_grades)):
-    students_grades[i] = list(randint(1, 100) for s in range(int(input(f'Введите количество студентов в {i + 1} группе :'))))
+    students_grades[i] = list(
+        randint(1, 100) for s in range(int(input(f'Введите количество студентов в {i + 1} группе :'))))
 
 print(students_grades)
+
+average_students = np.mean(students_grades, axis=1)
+
+print(np.round(average_students))
+
+lst = students_grades
+a = lst.index(max(lst)) + 1
+
+b = max(lst)
+c = lst.index(b)
+print(b)
+print(c + 1)
 
 
