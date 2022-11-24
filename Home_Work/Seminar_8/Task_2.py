65a3b7558048dac3b854361da9bc5614d2d966bb
unknown <ot250013@yandex.ru>
2022-11-25 01:36:39 +0300

    Task_2 OK

--- Home_Work/Seminar_8/Task_2.py
+++ Home_Work/Seminar_8/Task_2.py
@@ -11,56 +11,57 @@ matrix = [0] * size_matrix
 def create_matrix(size):
     for i in range(size):
         matrix[i] = list(randint(1, 100) for s in range(size))
-    print(f'квадратная матрица размером {size_matrix} * {size_matrix}: ')
     return matrix
 
 
-create_matrix(size_matrix)
-
-
-def sum_stroki(matrix):
-    A = np.array(matrix)
-    max_sum_stroki = []
+def sum_row(matrix):
+    a = np.array(matrix)
+    max_sum_row = []
     for i in range(len(matrix)):
-        m = (sum(A[i]))
-        max_sum_stroki.append(m)
-    return max_sum_stroki
+        m = (sum(a[i]))
+        max_sum_row.append(m)
+    return max_sum_row
 
 
-print('Сумма строк матрицы: ')
-print(sum_stroki(matrix))
-
-
-def diagonal():
+def sum_diagonal(mtr):
     count = 0
-    for i in range(size_matrix):
-        for j in range(size_matrix):
+    for i in range(len(mtr)):
+        for j in range(len(mtr)):
             if i == j:
-                count += matrix[i][j]
-    print(f' {count} диагональ')
+                count += mtr[i][j]
     return count
 
 
-print('Сумма главной диагонали: ')
-diagonal()
-
-
-def poisk():
-    for i in range(len(sum_stroki(matrix))):
-        if matrix[i] > sum_stroki(matrix):
-            print(matrix[i])
-            matrix[i]
-
-
-print(poisk())
+def res(diagonal, s_row):
+    s = list(filter(lambda x: x > diagonal, s_row))
+    print(f'Сумма строк превосходящих сумму главной диагонали {s}')
 
 
 def Print(matrix):
+    print(f'Матрица {size_matrix} * {size_matrix}: ')
     for i in range(size_matrix):
         for j in range(size_matrix):
             print(str(matrix[i][j]).rjust(3), end=' ')
         print()
+    print()
     return
 
 
+create_matrix(size_matrix)
 Print(matrix)
+print(f'Сумма строк матрицы: {sum_row(matrix)} ')
+array = sum_row(matrix)
+print(f'Сумма главной диагонали: {sum_diagonal(matrix)}')
+line = sum_diagonal(matrix)
+res(sum_diagonal(matrix), sum_row(matrix))
+
+
+def indx(lin, array):
+    d = [0]
+    for i in range(len(array)):
+        if array[i] > lin:
+            print(f'Строка с суммой [{array[i]}] стоит на ==>  [{array.index(array[i])+1}] строке')
+
+
+indx(line, array)
+
